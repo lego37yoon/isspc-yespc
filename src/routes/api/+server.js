68ca5e7e0d5df7d49fec35b0f61f5e0b6af41942
somni <me@somni.one>
2022-10-27 17:18:06 +0900

    Break loop when manufacturer has found

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -14,6 +14,7 @@ export function isSpcProduct(barcode) {
             returnStrings.manu = manu.name;
             returnStrings.spc = manu.spc;
             returnStrings.color = manu.color;
+            break;
         }
     }
 
