68c5dd9b326a0f1ce7cc9003bf2bfbf1b4c1f048
somni <me@somni.one>
2022-10-27 17:13:53 +0900

    Load manufacturers from JSON dynamically

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,36 +1,21 @@
+import manufacturers from "./manufacturers.json";
 
 export function isSpcProduct(barcode) {
-    let returnStrings = {
+    const returnStrings = {
         manu: null,
         spc: false,
         barcode: barcode,
         color: null
     };
 
-    if (
-        barcode.startsWith("8804007") ||
-        barcode.startsWith("880939312") ||
-        barcode.startsWith("8801068")
-    ) {
-        returnStrings.manu = "SPC삼립";
-        returnStrings.spc = true;
-        returnStrings.color = "#30B3E7";
-        return returnStrings;
-    } else if (
-        barcode.startsWith("880928863")
-    ) {
-        returnStrings.manu = "BR코리아";
-        returnStrings.spc = true;
-        returnStrings.color = "#DB5B9B";
-        return returnStrings;
-    } else if (
-        barcode.startsWith("880969023")
-    ) {
-        returnStrings.manu = "파리크라상";
-        returnStrings.spc = true;
-        returnStrings.color = "#4063A0";
-        return returnStrings;
-    } else {
-        return returnStrings;
+    for(const index of Object.keys(manufacturers.indexes)) {
+        if(barcode.startsWith(index)) {
+            const manu = manufacturers[manufacturers.indexes[index]];
+            returnStrings.manu = manu.name;
+            returnStrings.spc = manu.spc;
+            returnStrings.color = manu.color;
+        }
     }
+
+    return returnStrings;
 }
